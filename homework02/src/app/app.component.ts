582d57cc16d71304b68f47082fd442e6ac1120e3
ZoranK <zorankarbev@gmail.com>
2022-08-13 18:07:04 +0200

    added global styles and fetch teams data

--- homework02/src/app/app.component.ts
+++ homework02/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Baketball App';
+  constructor() { }
+
+  ngOnInit(): void {
+    fetch('https://www.balldontlie.io/api/v1/teams')
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+    })
+    .catch(error => console.log(error));
+  }
 }
