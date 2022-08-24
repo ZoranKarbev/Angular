6b119a8289b16eb9a7e6a356a69f76f1f79d6cbe
ZoranK <zorankarbev@gmail.com>
2022-08-25 00:43:33 +0200

    added recipes service

---
+++ homework03/src/app/services/recipes.service.ts
@@ -0,0 +1,61 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Recipe } from '../interfaces/Recipe';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipesService {
+  recipes: Recipe[] = [
+    {
+      id: 1,
+      name: 'Party Cake',
+      description: 'Nice coffee cake',
+      ingredients: ['flour', 'sugar', 'baking powder', 'salt', 'butter', 'eggs', 'milk'] 
+    },
+    {
+      id: 2,
+      name: 'BBB Cake',
+      description: 'Nice coffee cake',
+      ingredients: ['flour', 'sugar', 'baking powder', 'salt', 'butter', 'eggs', 'milk'] 
+    },
+    {
+      id: 3,
+      name: 'CCCC Cake',
+      description: 'Nice coffee cake',
+      ingredients: ['flour', 'sugar', 'baking powder', 'salt', 'butter', 'eggs', 'milk'] 
+    },
+    {
+      id: 4,
+      name: 'DDD Cake',
+      description: 'Nice coffee cake',
+      ingredients: ['flour', 'sugar', 'baking powder', 'salt', 'butter', 'eggs', 'milk'] 
+    },
+    {
+      id: 5,
+      name: 'EEEE Cake',
+      description: 'Nice coffee cake',
+      ingredients: ['flour', 'sugar', 'baking powder', 'salt', 'butter', 'eggs', 'milk'] 
+    },
+  ]
+
+  recipesBehaviourSubject = new BehaviorSubject<Recipe[]>(this.recipes);
+  $recipesObs = this.recipesBehaviourSubject as Observable<Recipe[]>;
+
+  constructor() { }
+
+  getAllRecipes() {
+    this.recipesBehaviourSubject.next([...this.recipes])
+  }
+  getRecipeById(id: number) {
+    const recipes = this.recipesBehaviourSubject.getValue();
+    const foundRecipe = recipes.filter(recipe => recipe.id === id)
+    this.recipesBehaviourSubject.next(foundRecipe)
+    
+  }
+  addRecipe(recipe: Recipe) {
+    const recipes = this.recipesBehaviourSubject.getValue();
+    this.recipesBehaviourSubject.next([...recipes, recipe])    
+  }
+}
