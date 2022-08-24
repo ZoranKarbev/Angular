b0e437134485c92b1e815a1eaa11fd89d247fdf4
ZoranK <zorankarbev@gmail.com>
2022-08-24 19:14:32 +0200

    added components

--- homework03/src/app/app.module.ts
+++ homework03/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AddRecipeComponent } from './components/add-recipe/add-recipe.component';
+import { RecipesComponent } from './components/recipes/recipes.component';
+import { RecipeDetailsComponent } from './components/recipe-details/recipe-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AddRecipeComponent,
+    RecipesComponent,
+    RecipeDetailsComponent
   ],
   imports: [
     BrowserModule
