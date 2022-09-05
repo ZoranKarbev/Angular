7283635bc45993025f27e9ea24eccfe9ba868715
ZoranK <zorankarbev@gmail.com>
2022-09-05 16:45:54 +0200

    added people view and styles

--- workshop01/src/app/features/people/components/people-list/people-list.component.ts
+++ workshop01/src/app/features/people/components/people-list/people-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { SwapiService } from 'src/app/services/swapi.service';
+import { Person } from "src/app/interfaces/person";
 
 @Component({
   selector: 'app-people-list',
@@ -7,14 +9,25 @@ import { SwapiService } from 'src/app/services/swapi.service';
   styleUrls: ['./people-list.component.scss']
 })
 export class PeopleListComponent implements OnInit {
+  people: Person[];
+  showLoader: boolean = true;
+  constructor(private peopleService: SwapiService, private router: Router) { }
 
-  constructor(private peopleService: SwapiService) { }
-
-  get peopleObs$() {
-    return this.peopleService.peopleObs$;
-  }
+  // get peopleObs$() {
+  //   return this.peopleService.peopleObs$;
+  // }
 
   ngOnInit(): void {
     this.peopleService.getPeople();
+    this.peopleService.peopleObs$.subscribe({
+      next: (people) => (this.people = people),
+    })
+  }
+
+  onPersonClick(person: Person) {
+    console.log("Person", person);
+    this.peopleService.onPersonSelect(person);
+    const id = person.url.split("/").slice(-2)[0];
+    this.router.navigate(["person/details", id])
   }
 }
